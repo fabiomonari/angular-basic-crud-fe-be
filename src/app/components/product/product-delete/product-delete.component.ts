83a8856e38a727324ea037c0eb80d5e4dd8b7aab
Fábio Monari <fabiomonari@gmail.com>
2022-02-27 20:32:26 -0300

    Adding Delete Component

--- src/app/components/product/product-delete/product-delete.component.ts
+++ src/app/components/product/product-delete/product-delete.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { Product } from '../product.model';
 import { ProductService } from '../product.service';
 
 @Component({
-  selector: 'app-product-update',
-  templateUrl: './product-update.component.html',
-  styleUrls: ['./product-update.component.css'],
+  selector: 'app-product-delete',
+  templateUrl: './product-delete.component.html',
+  styleUrls: ['./product-delete.component.css'],
 })
-export class ProductUpdateComponent implements OnInit {
+export class ProductDeleteComponent implements OnInit {
   product!: Product;
 
   constructor(
@@ -24,11 +24,13 @@ export class ProductUpdateComponent implements OnInit {
     });
   }
 
-  updateProduct(): void {
-    this.productService.update(this.product).subscribe(() => {
-      this.productService.showMessage('Produto atualizado com sucesso!');
-      this.router.navigate(['/products']);
-    });
+  deleteProduct(): void {
+    if (this.product.id) {
+      this.productService.delete(this.product.id).subscribe(() => {
+        this.productService.showMessage('Produto excluído com sucesso!');
+        this.router.navigate(['/products']);
+      });
+    }
   }
 
   cancel(): void {
